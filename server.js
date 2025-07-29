e626aecf139db170dbe2533c888fdd27c117b28f
Sooraj <soorajrsuresh@gmail.com>
2025-07-29 15:07:04 +0530

    server added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+// Connect DB
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api', require('./routes/admin'));
+// ... other routes
+
+// Start Server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
